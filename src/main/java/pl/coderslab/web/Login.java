1b5db5916bb746e01f3362d585e11a8f35b9a283
agnieszkagregosiewicz <agnieszka.gregosiewicz@gmail.com>
2022-10-11 13:31:24 +0200

    LogForm add

---
+++ src/main/java/pl/coderslab/web/Login.java
@@ -0,0 +1,21 @@
+package pl.coderslab.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "Login", value = "/login")
+public class Login extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        getServletContext().getRequestDispatcher("/login.html").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
