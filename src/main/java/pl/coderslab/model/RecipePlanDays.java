2c58bba6308dfb45d421deaf1840e8a9e9de8e1f
agnieszkagregosiewicz <agnieszka.gregosiewicz@gmail.com>
2022-10-13 17:10:32 +0200

    planDao upgrade

---
+++ src/main/java/pl/coderslab/model/RecipePlanDays.java
@@ -0,0 +1,32 @@
+package pl.coderslab.model;
+
+public class RecipePlanDays {
+
+    private String dayName;
+    private String mealName;
+    private String recipeName;
+
+    public String getDayName() {
+        return dayName;
+    }
+
+    public void setDayName(String dayName) {
+        this.dayName = dayName;
+    }
+
+    public String getMealName() {
+        return mealName;
+    }
+
+    public void setMealName(String mealName) {
+        this.mealName = mealName;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+}
